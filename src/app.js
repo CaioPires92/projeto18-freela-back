52e5993cf892dc1d9b3f850308baa2a672f34242
Caio Pires <caiogodoi_92@hotmail.com>
2023-08-08 14:54:08 -0300

    :art: Structure base of API

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
-import express, { json } from 'express'
+import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import router from './routes/index.routes.js'
+// import router from './routes/index.routes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
-app.use(express, json())
-app.use(router)
+app.use(express.json())
+// app.use(router)
+
+app.get('/teste', (req, res) => {
+  return res.send('Funcionoooouuuuuuuu')
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`))
